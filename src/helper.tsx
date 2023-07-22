6edcc9a448f0bc77965f64eb0bcca91c377e931c
Alasdair Keddie <alasdair@digitise.co.uk>
2023-07-22 07:52:54 +0100

    Update helper.tsx

--- src/helper.tsx
+++ src/helper.tsx
@@ -1,7 +1,7 @@
 
 export class Urls {
     public static  SignUp = 'https://docs.google.com/forms/d/e/1FAIpQLScJpHQ2sVjTQyvx_ysCsuBIykouZsfMumQlKgzbOa8Zuoh3LA/viewform?usp=sf_link';
-    public static  GitHub = 'https://github.com/PaulBullivant/queens-park-community';
+    public static  GitHub = 'https://github.com/digitiser/cyrilpark';
     public static  Code = 'code';
     public static  Privacy = 'privacy';
 }
