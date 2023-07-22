7044a698499ad6f6fa8e0be1ff67a2b7975a48f7
Alasdair Keddie <alasdair@digitise.co.uk>
2023-07-22 18:53:24 +0100

    Update helper.tsx
    google form link

--- src/helper.tsx
+++ src/helper.tsx
@@ -1,6 +1,6 @@
 
 export class Urls {
-    public static  SignUp = 'https://docs.google.com/forms/d/e/1FAIpQLScJpHQ2sVjTQyvx_ysCsuBIykouZsfMumQlKgzbOa8Zuoh3LA/viewform?usp=sf_link';
+    public static  SignUp = 'https://docs.google.com/forms/d/e/1FAIpQLScIa_s2OVWOK45eMJKCHto4EGAsZLmzJs3gjbMZULBdiEu-rg/viewform?usp=sf_link';
     public static  GitHub = 'https://github.com/digitiser/cyrilpark';
     public static  Code = 'code';
     public static  Privacy = 'privacy';
